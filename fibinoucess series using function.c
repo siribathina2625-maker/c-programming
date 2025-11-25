a6276b7a61e21c0c795722757be9504cfa59fd52
siribathina2625-maker <siribathina2625@gmail.com>
2025-11-25 15:13:09 +0530

    Add files via upload

---
+++ fibinoucess series using function.c
@@ -0,0 +1,22 @@
+void fibo(int);
+int main()
+{
+	int n;
+	printf("enter size");
+	scanf("%d",&n);
+	fibo(n);
+	return 0;
+}
+ void fibo (int n)
+{
+	int f=0,s=1,t;
+	printf("%d%d",f,s);
+	t=f+s;
+	while(t<=n)
+	{
+		printf("%d",t);
+		f=s;
+		s=t;
+		t=f+s;
+	}
+}
