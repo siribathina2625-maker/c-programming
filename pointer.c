c96066dc4fc780a7dc5dc0d510e40a13ebbb2516
siribathina2625-maker <siribathina2625@gmail.com>
2025-12-02 23:15:01 +0530

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,10 @@
+int main()
+{
+	int a=10,*p;
+	p=&a;
+	printf("value of a=%d",a);
+	printf("\n adress of a=%u",p);
+	printf("\n adress of p=%u",&p);
+	printf("\n adress of p=%d",*p);
+	return 0;
+}
